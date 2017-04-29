9d0284064ec4870ed8be2311ca49aa03aa441122
James Jannicelli <jjannicelli@gmail.com>
2017-04-29 12:26:10 -0400

    issue 31 comments

--- tests/common.py
+++ tests/common.py
@@ -5,4 +5,8 @@ import unittest
 class CustomTestCase(unittest.TestCase):
 
     def assertListsEqual(self, l1, l2):
+        """
+        Convenience method for asserting that the contents
+        of two lists are the same, regardless of order.
+        """
         self.assertEqual(Counter(l1), Counter(l2))
