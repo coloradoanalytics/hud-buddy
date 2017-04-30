7dee9263868105754c74b080bd5a1edd4aac9e42
James Jannicelli <jjannicelli@gmail.com>
2017-04-30 14:17:17 -0400

    tests for site processing

--- tests/test_sites.py
+++ tests/test_sites.py
@@ -1,6 +1,6 @@
 from tests.common import CustomTestCase
 
-from locations import Position
+from locations import Position, County, Population
 from highways import Road
 from sites import Site
 
@@ -38,3 +38,39 @@ class UniqueTestCase(CustomTestCase):
         site.remove_duplicates()
 
         self.assertListsEqual(site.roads, [r1, r3])
+
+
+class SiteProcessTestCase(CustomTestCase):
+
+    def setUp(self):
+        self.r1 = Road(
+            name="I-25", positions=[Position(lat=39.1212, lng=-104.012)])
+        self.r2 = Road(
+            name="I-70", positions=[Position(lat=39.2424, lng=-104.345)])
+
+        p1 = Population(population=1000, year=2014, county="Denver")
+        p2 = Population(population=2000, year=2027, county="Denver")
+
+        county = County(
+            current_population=p1,
+            future_population=p2,
+            name="Denver"
+        )
+
+        self.site = Site(roads=[self.r1, self.r2],
+                         position=Position(lat=39.1213, lng=-104.013),
+                         county=county)
+
+    def test_set_growth_rates(self):
+        self.site.set_growth_rates()
+
+        rate = self.site.growth_rate
+        for r in self.site.roads:
+            self.assertAlmostEqual(r.growth_rate, rate, 3)
+
+    def test_set_distances(self):
+        self.site.set_distances()
+        self.assertEqual(self.r1.distance, self.r1.positions[
+                         0].distance_from(self.site.position))
+        self.assertEqual(self.r2.distance, self.r2.positions[
+                         0].distance_from(self.site.position))
