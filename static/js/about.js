c01b92561fd842341962ab88f9b6adba37c74cb7
Joshua Leasure <joshua@coloradoanalytics.com>
2017-05-05 15:51:21 -0600

    started about page and site summary

--- static/js/about.js
+++ static/js/about.js
@@ -1,10 +1,16 @@
 var AboutTab = {
 
 	template: `
-		<div class="section">
-      <div class="container">
-        About HUDL
+	 <div class="section">
+    <div class="container">
+      <div class="heading">
+        <h1 class="title">What is HUDL?</h1>
       </div>
+      HUDL is a tool that makes doing HUD noise studies much simpler.<br><br>
+
+      HUDL is one of 10 finalists in the 2017 <a>GoCode Colorado</a> business and software development contest. On May 24th
+      we will present this application to an audience and a panel of judges.
     </div>
+  </div>
   `
 }
