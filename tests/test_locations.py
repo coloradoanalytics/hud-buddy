c2e436591eec60faab56b47664432e391bda3daf
James Jannicelli <jjannicelli@gmail.com>
2017-05-20 11:36:34 -0400

    add comment to growth rate

--- tests/test_locations.py
+++ tests/test_locations.py
@@ -6,6 +6,11 @@ from locations.models import Population, County
 class GrowthRateTestCase(CustomTestCase):
 
     def test_get_growth_rate(self):
+        """
+        Assert that the counties growth rate is being
+        calculated correctly, given a past and future
+        population count.
+        """
         p1 = Population(150000, 2014, "denver")
         p2 = Population(250000, 2027, "denver")
 
